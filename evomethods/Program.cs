b313ddcb9d44e6b531e2b2cc1bd6ce6f67299ced
Артемий Андреевич Иванов <ghostsmans@gmail.com>
2022-04-28 22:46:34 +0300

    -

--- evomethods/Program.cs
+++ evomethods/Program.cs
@@ -5,11 +5,148 @@ namespace evomethods
 {
     class Program
     {
+        protected class Gamet : IComparable<Gamet>
+        {
+            public string rbcvalue;
+            public double fitness;
+            public Gamet(string bininp)
+            {
+                rbcvalue = binaryToRBC(bininp);
+                fitness = double.MaxValue / 2;
+            }
+
+            public void printValue()
+            {
+                Console.WriteLine(rbcvalue);
+            }
+
+            public int CompareTo(Gamet other)
+            {
+                return -fitness.CompareTo(other.fitness);
+            }
+        }
+
+        protected class Population
+        {
+            public Gamet[] curPop;
+            // Количество особей
+            public int popSize;
+            private Dictionary<string, double>[] valueLibrary;
+            private List<string>[] keyLibrary;
+            // Длина одной переменной в коде Грея
+            public int fragmentSize;
+            public Population(double[][] inp0, int popSize)
+            {
+                this.popSize = popSize;
+                var rand = new Random();
+                valueLibrary = new Dictionary<string, double>[5];
+                keyLibrary = new List<string>[5];
+                for (int i = 0; i < 5; i++)
+                {
+                    Dictionary<string, double> cur = toPopulation(inp0[i][0], inp0[i][1], inp0[i][2]);
+                    keyLibrary[i] = new List<string>(cur.Keys);
+                    valueLibrary[i] = cur;
+                }
+                fragmentSize = keyLibrary[0][0].Length;
+                curPop = new Gamet[popSize];
+                for (int i = 0; i < popSize; i++)
+                {
+                    string temp = "";
+                    for (int j = 0; j < 5; j++)
+                    {
+                        int key = rand.Next(keyLibrary[j].Count);
+                        temp += keyLibrary[j][key];
+                    }
+                    curPop[i] = new Gamet(temp);
+                }
+            }
+
+            public void fitnessTest(Func<double[], double> fitness)
+            {
+                double[] temp = new double[5];
+                double[] output = new double[popSize];
+                for (int i = 0; i < popSize; i++)
+                {
+                    string tempString = curPop[i].rbcvalue;
+                    tempString = RBCtoBinary(tempString);
+                    for (int j = 0; j < 5; j++)
+                    {
+                        temp[j] = valueLibrary[j][tempString.Substring(j * fragmentSize, fragmentSize)];
+                    }
+                    output[i] = fitness(temp);
+                    curPop[i].fitness = output[i];
+                }
+                Array.Sort(curPop);
+            }
+
+            public double[] getGametValue(int index)
+            {
+                double[] output = new double[5];
+                string tempString = curPop[index].rbcvalue;
+                tempString = RBCtoBinary(tempString);
+                for (int j = 0; j < 5; j++)
+                {
+                    output[j] = valueLibrary[j][tempString.Substring(j * fragmentSize, fragmentSize)];
+                }
+                return output;
+            }
+
+            public void outputPopulation()
+            {
+                foreach (Gamet cur in curPop)
+                {
+                    cur.printValue();
+                }
+            }
+
+            public void outputFitness()
+            {
+                foreach(Gamet cur in curPop)
+                {
+                    Console.Write(String.Format("{0:0.00}", cur.fitness) + '\t');
+                }
+                Console.WriteLine();
+            }
+
+            public double[] outputDif(int index)
+            {
+                double[] dif = { 7, -3, -12, -1, -6};
+                double[] output = new double[5];
+                string tempString = curPop[index].rbcvalue;
+                tempString = RBCtoBinary(tempString);
+                for (int j = 0; j < 5; j++)
+                {
+                    output[j] = Math.Abs(valueLibrary[j][tempString.Substring(j * fragmentSize, fragmentSize)]+dif[j]);
+                }
+                return output;
+            }
+        }
+
+        public class Operator
+        {
+            public double prob;
+            bool mode;
+            public Func<string[], string[]> operationt;
+            public Func<string, string> operationf;
+            public Operator(Func<string[], string[]> operation, double prob)
+            {
+                mode = true;
+                this.operationt = operation;
+                this.prob = prob;
+            }
+            public Operator(Func<string, string> operation, double prob)
+            {
+                mode = false;
+                this.operationf = operation;
+                this.prob = prob;
+            }
+        }
 
         public static double fitness(double[] inp)
         {
             return Math.Pow(inp[0] + 7, 2) + Math.Pow(inp[1] - 3, 2) + Math.Pow(inp[2] - 12, 2) + Math.Pow(inp[3] - 1, 2) + Math.Pow(-inp[4] + 6, 2);
         }
+
         public static bool checkBinary(string inp)
         {
             foreach (char symbol in inp)
@@ -21,6 +158,7 @@ namespace evomethods
             }
             return true;
         }
+
         public static string binaryToRBC(string inp)
         {
             if (!checkBinary(inp))
@@ -55,6 +193,7 @@ namespace evomethods
             }
             return output;
         }
+
         protected static Dictionary<string, double> toPopulation(double xMin, double xMax, double epsilon)
         {
             double nb = 1 + (xMax - xMin) / epsilon;
@@ -74,165 +213,164 @@ namespace evomethods
             return output;
         }
 
-        protected class Gamet
+        public static string Mutation(string rbcvalue, int position)
         {
-            public string rbcvalue;
-            public Gamet(string bininp)
-            {
-                this.rbcvalue = binaryToRBC(bininp);
-            }
-
-            public void printValue()
-            {
-                Console.WriteLine(rbcvalue);
-            }
-
-            // Однопозиционная мутация
-            public void Mutation(int position)
-            {
-                string temp = "";
-                temp += rbcvalue.Substring(0, position);
-                temp += rbcvalue[position] == '0' ? '1' : '0';
-                temp += rbcvalue.Substring(position + 1);
-                rbcvalue = temp;
-            }
-
-            // Многопозиционная мутация
-            public void Mutation(int[] position)
-            {
-                string temp = "";
-                int posAmount = position.Length;
-                temp += rbcvalue.Substring(0, position[0]);
-                for (int i = 0; i < posAmount - 1; i++)
-                {
-                    temp += rbcvalue[position[i]] == '0' ? '1' : '0';
-                    temp += rbcvalue.Substring(position[i] + 1, position[i + 1]-1);
-                }
-                temp += rbcvalue[position[posAmount-1]] == '0' ? '1' : '0';
-                temp += rbcvalue.Substring(position[posAmount-1] + 1);
-                rbcvalue = temp;
-            }
-
-            // Инверсия по заданной позиции
-            public void Inversion(int position)
-            {
-                string temp = "";
-                temp += rbcvalue.Substring(position);
-                temp += rbcvalue.Substring(0, position);
-                rbcvalue = temp;
-            }
-
-            // Кроссовер
-            public void Crossover(Gamet secondParent, int position)
-            {
-                string temp1 = rbcvalue.Substring(0, position);
-                string temp2 = secondParent.rbcvalue.Substring(0, position);
-                temp1 += secondParent.rbcvalue.Substring(position);
-                temp2 += rbcvalue.Substring(position);
-                rbcvalue = temp1;
-                secondParent.rbcvalue = temp2;
-            }
-
-            // Многопозиционный кроссовер
-            public void Crossover(Gamet secondParent, int[] position)
-            {
-                string rbcvalue1 = rbcvalue;
-                string rbcvalue2 = secondParent.rbcvalue;
-                string temp1 = rbcvalue1.Substring(0, position[0]-1);
-                string temp2 = rbcvalue2.Substring(0, position[0]-1);
-                string temp = rbcvalue1;
-                rbcvalue1 = rbcvalue2;
-                rbcvalue2 = temp;
-                for (int i = 0; i < position.Length-1; i++)
-                {
-                    temp1 += rbcvalue1.Substring(position[i], position[i + 1] - position[i]);
-                    temp2 += rbcvalue2.Substring(position[i], position[i + 1] - position[i]);
-                    temp = rbcvalue1;
-                    rbcvalue1 = rbcvalue2;
-                    rbcvalue2 = temp;
-                }
-                temp1 += rbcvalue1.Substring(position[position.Length - 1] - 1);
-                temp2 += rbcvalue2.Substring(position[position.Length - 1] - 1);
-                rbcvalue = temp1;
-                secondParent.rbcvalue = temp2;
-            }
+            string temp = "";
+            temp += rbcvalue.Substring(0, position);
+            temp += rbcvalue[position] == '0' ? '1' : '0';
+            temp += rbcvalue.Substring(position + 1);
+            return temp;
         }
 
-        protected class Population
+        public static string Inversion(string rbcvalue, int position)
         {
-            private Gamet[] curPop;
-            private int popSize;
-            private Dictionary<string, double>[] valueLibrary;
-            private List<string>[] keyLibrary;
-            private int fragmentSize;
-            public Population(double[][] inp0, int popSize)
-            {
-                this.popSize = popSize;
-                var rand = new Random();
-                valueLibrary = new Dictionary<string, double>[5];
-                keyLibrary = new List<string>[5];
-                for (int i = 0; i < 5; i++)
-                {
-                    Dictionary<string, double> cur = toPopulation(inp0[i][0], inp0[i][1], inp0[i][2]);
-                    keyLibrary[i] = new List<string>(cur.Keys);
-                    valueLibrary[i] = cur;
-                }
-                fragmentSize = keyLibrary[0][0].Length;
-                curPop = new Gamet[popSize];
-                for (int i = 0; i < popSize; i++)
-                {
-                    string temp = "";
-                    for (int j = 0; j < 5; j++)
-                    {
-                        int key = rand.Next(keyLibrary[i].Count);
-                        temp += keyLibrary[i][key];
-                    }
-                    curPop[i] = new Gamet(temp);
-                }
-            }
-
-            public double[] fitnessTest(Func<double[], double> fitness)
-            {
-                double[] temp = new double[5];
-                double[] output = new double[popSize];
-                for (int i = 0; i < popSize; i++)
-                {
-                    string tempString = curPop[i].rbcvalue;
-                    tempString = RBCtoBinary(tempString);
-                    for (int j = 0; j < 5; j++)
-                    {
-                        temp[j] = valueLibrary[j][tempString.Substring(j * fragmentSize, fragmentSize)];
-                    }
-                    output[i] = fitness(temp);
-                    Console.WriteLine(string.Format("({0}+7)^2+({1}-3)^2+({2}-12)^2+({3}-1)^2+(-{4}+6)^2 = {5}", temp[0], temp[1], temp[2], temp[3], temp[4], output[i]));
-                }
-
-                return output;
-            }
-
-            public void outputPopulation()
-            {
-                foreach(Gamet cur in curPop)
-                {
-                    cur.printValue();
-                }
-            }
+            string temp = "";
+            temp += rbcvalue.Substring(position);
+            temp += rbcvalue.Substring(0, position);
+            return temp;
         }
+
+        public static string[] Crossover(string[] rbcvalues, int position)
+        {
+            string rbcvalue1 = rbcvalues[0], rbcvalue2 = rbcvalues[1];
+            string temp1 = rbcvalue1.Substring(0, position);
+            string temp2 = rbcvalue2.Substring(0, position);
+            temp1 += rbcvalue1.Substring(position);
+            temp2 += rbcvalue2.Substring(position);
+            rbcvalue1 = temp1;
+            rbcvalue2 = temp2;
+            return new string[]{ rbcvalue1, rbcvalue2 };
+        }
+
+        public static string Translocate(string rbcvalue, int[] index)
+        {
+
+            string output = rbcvalue.Substring(0, index[0]);
+            string toShake = rbcvalue.Substring(index[0], index[1] - index[0]);
+            string tail = rbcvalue.Substring(index[1]);
+            toShake = Shuffle(toShake);
+            output = output + toShake + tail;
+            return output;
+        }
+
+        public static string Shuffle(string str)
+        {
+            char[] array = str.ToCharArray();
+            Random rng = new Random();
+            int n = array.Length;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                var value = array[k];
+                array[k] = array[n];
+                array[n] = value;
+            }
+            string output = new string(array);
+            return new string(array);
+        }
+
+        public static int[] Check(int[] ind)
+        {
+            if (ind[0] > ind[1])
+            {
+                ind[1] += ind[0];
+                ind[0] = ind[1] - ind[0];
+                ind[1] -= ind[0];
+            }
+            return ind;
+        }
+
         static void Main(string[] args)
         {
-            double[] pos1 = { -10, -4, 0.5 };
-            double[] pos2 = { 0, 6, 0.5 };
-            double[] pos3 = { 9, 15, 0.5 };
-            double[] pos4 = { -2, 4, 0.5 };
-            double[] pos5 = { 3, 9, 0.5 };
+            Random rand = new Random();
+            int startcount = 40;
+            double eps = 0.01;
+            double[] pos1 = { -10, -4, eps };
+            double[] pos2 = { 0, 6, eps };
+            double[] pos3 = { 9, 15, eps };
+            double[] pos4 = { -2, 4, eps };
+            double[] pos5 = { 3, 9, eps };
             double[][] inp0 = { pos1, pos2, pos3, pos4, pos5 };
-            Population test = new Population(inp0, 4);
-            test.outputPopulation();
-            double[] fit = test.fitnessTest(fitness);
-            foreach(double val in fit)
+            int segcount = 0;
+            int fullcount = 0;
+            for (int i = 0; i < 100000; i++)
             {
-                Console.WriteLine(val);
+                if (i%1000 == 0)
+                {
+                    Console.WriteLine("Тест №" + i.ToString());
+                }
+                Population testFull = new Population(inp0, 1);
+                Population testSeg = new Population(inp0, 1);
+                testFull.fitnessTest(fitness);
+                double fit1 = testFull.curPop[0].fitness;
+                int segNumb = rand.Next(1, 5);
+                int indFull = rand.Next(testFull.fragmentSize * 5);
+                string rbcInp = testFull.curPop[0].rbcvalue;
+                string toInverse = rbcInp.Substring(segNumb, testFull.fragmentSize);
+                string inversed = Inversion(toInverse, rand.Next(testFull.fragmentSize));
+                string checkFull = Inversion(rbcInp, indFull);
+                string checkSeg = rbcInp.Substring(0, segNumb) +inversed+rbcInp.Substring(segNumb + testFull.fragmentSize);
+                testFull.curPop[0].rbcvalue = checkFull;
+                testSeg.curPop[0].rbcvalue = checkSeg;
+                testFull.fitnessTest(fitness);
+                testSeg.fitnessTest(fitness);
+                if (testFull.curPop[0].fitness < fit1) 
+                {
+                    fullcount++;
+                }
+                if (testSeg.curPop[0].fitness < fit1) 
+                {
+                    segcount++;
+                }
             }
+            Console.WriteLine("Процент улучшений при транслокации на всей гамете: " + ((double)(fullcount / 1000)).ToString());
+            Console.WriteLine("Процент улучшений при транслокации на сегменте: " + ((double)(segcount / 1000)).ToString());
+
+            /*int startcount = 40;
+            double eps = 0.01;
+            double[] pos1 = { -10, -4, eps };
+            double[] pos2 = { 0, 6, eps };
+            double[] pos3 = { 9, 15, eps };
+            double[] pos4 = { -2, 4, eps };
+            double[] pos5 = { 3, 9, eps };
+            double[][] inp0 = { pos1, pos2, pos3, pos4, pos5 };
+            Operator op1 = new Operator(Mutation, 0.6);
+            Operator op2 = new Operator(Inversion, 0.2);
+            Operator op3 = new Operator(Crossover, 0.2);
+            Operator[] ops = { op1, op2, op3 };
+
+
+            Population test = new Population(inp0, 10);
+            test.fitnessTest(fitness);
+
+
+            double bestFitness = test.curPop[test.popSize - 1].fitness;
+            int count = startcount;
+            while (test.curPop[test.popSize-1].fitness > eps && count > 0)
+            {
+                count--;
+                ApplyOperatorsToPop(test, ops, fitness);
+                test.outputFitness();
+                if (bestFitness > test.curPop[test.popSize - 1].fitness)
+                {
+                    bestFitness = test.curPop[test.popSize - 1].fitness;
+                    count = startcount;
+                }
+            }
+            double[] fin = test.getGametValue(test.popSize - 1);
+            foreach (double cur in fin)
+            {
+                Console.Write(String.Format("{0:0.00}", cur) + '\t');
+            }
+            Console.Write("\tНайденные значения");
+            Console.WriteLine();
+            fin = test.outputDif(test.popSize - 1);
+            foreach (double cur in fin)
+            {
+                Console.Write(String.Format("{0:0.00}", cur) + '\t');
+            }
+            Console.Write("\tОтклонения от истинных значений");*/
         }
     }
 }
